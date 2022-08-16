a013efdf1d645fd4d671736518f91f4e45477eb7
Tertychnyi <rett8716@gmail.com>
2022-08-16 18:47:06 +0200

    add button fixing

--- src/redux/reducers/cartReducer.tsx
+++ src/redux/reducers/cartReducer.tsx
@@ -1,12 +1,23 @@
-import {ICart,clothesCart} from "../../types";
-
+import {ICart} from "../../types";
 const defaultState:ICart = {
     cart:[],
+}
+function getProper(state:any,action:any) {
+    return state.cart.find((item:any)=>item.id === action.payload.id)
 }
 export const cartReducer = (state=defaultState,action:any) => {
     switch (action.type) {
         case "CART__ADD" :
-            return {...state, cart : [...state.cart , {...action.payload,count:1} ]}
+            const findProperItem = getProper(state,action)
+            if(findProperItem) {
+                findProperItem.count++
+                return {...state}
+            } else {
+                return {
+                    ...state,
+                    cart: [...state.cart , {...action.payload , count:1} ]
+                }
+            }
         default:
             return state
     }
